f6aed4f01fc620a6b7cb4d74c68221b877c8702e
yosepjeon <enekelx1@naver.com>
2021-05-05 23:45:48 +0900

    UserEntity 추가

---
+++ src/main/java/com/yosep/user/data/entity/UserEntity.java
@@ -0,0 +1,12 @@
+package com.yosep.user.data.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Data
+public class UserEntity {
+}
